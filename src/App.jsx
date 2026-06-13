89379cc120a78bd00d8cf510f1c54f23226cda0b
Tiago <tiagoferlacamini13@gmail.com>
2026-06-13 17:43:21 -0300

    place all

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <nav>
+
+        </nav>
+      </header>
+    </>
+  )
+
+}
+
+export default App
